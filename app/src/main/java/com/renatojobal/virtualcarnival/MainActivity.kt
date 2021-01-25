81f76f9230daaef9fead6fd99c8245c3f7e21829
renatojobal <renatojobal@gmail.com>
2021-01-25 11:37:31 -0500

    fix black background in some devices

--- app/src/main/java/com/renatojobal/virtualcarnival/MainActivity.kt
+++ app/src/main/java/com/renatojobal/virtualcarnival/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.renatojobal.virtualcarnival
 
 import android.graphics.drawable.Drawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +16,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Hide the status bar.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
+        // status bar is hidden, so hide that too if necessary.
+        actionBar?.hide()
+
 
         // Create an array with the drawables
         var bombs = arrayListOf<Drawable>()
